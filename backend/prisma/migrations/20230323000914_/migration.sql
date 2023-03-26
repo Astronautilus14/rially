b0710883227b26decbe25d1eb3406c8821f90349
Naut de Vroome <nautdevroome@gmail.com>
2023-03-26 01:37:22 +0100

    Dockerfile poging87

--- backend/prisma/migrations/20230323000914_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isCommitte` on the `team` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `team` DROP COLUMN `isCommitte`,
-    ADD COLUMN `isCommittee` BOOLEAN NOT NULL DEFAULT false;
