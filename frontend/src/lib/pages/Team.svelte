a2126308e5b8de4e7dec06ea84617f0e149ea07c
Daniel Jonker <ddavidjonker@outlook.com>
2023-03-26 22:52:12 +0200

    teams look more geil

--- frontend/src/lib/pages/Team.svelte
+++ frontend/src/lib/pages/Team.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { navigate } from "svelte-routing";
-  import settings from "../settings.json"
+  import GlassCard from "../../components/GlassCard.svelte";
+  import settings from "../settings.json";
 
   export let id: string;
   let error = "";
@@ -11,162 +12,197 @@
   onMount(() => {
     fetch(`${settings.api_url}/teams/${id}`, {
       headers: {
-        Authorization: localStorage.getItem("rially::token")
-      }
+        Authorization: localStorage.getItem("rially::token"),
+      },
     })
-    .then(res => res.json())
-    .then((data) => {
-      team = data;
-    })
-    .catch((e) => error = e)
-    .finally(() => loading = false)
+      .then((res) => res.json())
+      .then((data) => {
+        team = data;
+      })
+      .catch((e) => (error = e))
+      .finally(() => (loading = false));
   });
 
   let loadingTeamDelete = false;
   function handleTeamDelete() {
-    loadingTeamDelete = true
+    loadingTeamDelete = true;
     fetch(`${settings.api_url}/teams`, {
       headers: {
         Authorization: localStorage.getItem("rially::token"),
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       method: "DELETE",
       body: JSON.stringify({
-        teamId: team.id
+        teamId: team.id,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          navigate("/teams", { replace: true });
+          return;
+        }
+        res.json().then((data) => (error = data.message));
       })
-    })
-    .then(res => {
-      if (res.ok) {
-        navigate("/teams", { replace: true })
-        return;
-      };
-      res.json().then(data => error = data.message)
-    })
-    .catch((e) => error = e)
-    .finally(() => loadingTeamDelete = false)
+      .catch((e) => (error = e))
+      .finally(() => (loadingTeamDelete = false));
   }
 
-  let loadingMemberDeletes = {}
+  let loadingMemberDeletes = {};
   function handleMemberDelete(id: number) {
-    loadingMemberDeletes[id] = true
+    loadingMemberDeletes[id] = true;
     fetch(`${settings.api_url}/teams/member`, {
       headers: {
         Authorization: localStorage.getItem("rially::token"),
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       method: "DELETE",
       body: JSON.stringify({
-        userId: id
+        userId: id,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return;
+        res.json().then((data) => (error = data.message));
       })
-    })
-    .then(res => {
-      if (res.ok) return;
-      res.json().then(data => error = data.message)
-    })
-    .catch((e) => error = e)
-    .finally(() => {
-      delete loadingMemberDeletes[id]
-      error = ""
-    })
+      .catch((e) => (error = e))
+      .finally(() => {
+        delete loadingMemberDeletes[id];
+        error = "";
+      });
   }
 </script>
 
-<main>
-  <h1>{loading ? "Loading..." : team?.name}</h1>
-  <hr>
-  {#if loading}
-    <p>Loading...</p>
-  {:else}
-    {#if error}
-      <p class="error">{error}</p>
-    {/if}
-    <h2>Team members</h2>
-    {#if team.members.length === 0}
-      <p>This team has no members yet</p>
-    {/if}
-    {#each team.members as member}
-    <div class="user">
-      <p>Username: {member.username}</p>
-      <p>Name: {member.name}</p>
-      {#if member.discordId}
-      <p>Discord ID: {member.discordId}</p>
-      {:else}
-      <p>Discord not connected</p>
-      {/if}
-      {#if !team.isCommittee}
-      <button
-        class="delete"
-        on:click={() => handleMemberDelete(member.id)}
-      >
-        {loadingMemberDeletes[member.id] ? "Loading..." : "Delete"}
-      </button>
-      {/if}
-    </div>
-    {/each}
+<main class="contianer">
+  <div class="row justify-content-md-center">
+    <div class="col-12 col-sm-10">
+      <GlassCard title={loading ? "Loading..." : team?.name}>
+        <div class="row">
+          {#if loading}
+            <p>Loading...</p>
+          {:else}
+            {#if error}
+              <p class="error">{error}</p>
+            {/if}
+            <div class="col-6">
+              <h2>Team members</h2>
+              {#if team.members.length === 0}
+                <p>This team has no members yet</p>
+              {:else}
+                <table class="table table-striped table-bordered border-white">
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Username</th>
+                      <th scope="col">Discord ID</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {#each team.members as member}
+                      <tr>
+                        <td>{member.name}</td>
+                        <td>{member.username}</td>
+                        <td>
+                          {member.discordId
+                            ? member.discordId
+                            : "Not connected"}
+                        </td>
+                        <td>
+                          {#if !team.isCommittee}
+                            <button
+                              class="delete btn btn-danger"
+                              on:click={() => handleMemberDelete(member.id)}
+                            >
+                              {loadingMemberDeletes[member.id]
+                                ? "Loading..."
+                                : "Delete"}
+                            </button>
+                          {/if}
+                        </td>
+                      </tr>
+                    {/each}
+                  </tbody>
+                </table>
+              {/if}
+            </div>
 
-    <h2>Data</h2>
-    <div class="data">
-      <p>ID: {id}</p>
-      <p>isCommittee: {team.isCommittee.toString()}</p>
-      {#if team.channelId && team.roleId}
-      <p>Channel ID: {team.channelId}</p>
-      <p>Role ID: {team.roleId}</p>
-      {:else}
-      <p>Discord not connected</p>
-      {/if}
-    </div>
+            <div class="col-2">
+              <h2>Data</h2>
+              <div class="data">
+                <p>ID: {id}</p>
+                <p>isCommittee: {team.isCommittee.toString()}</p>
+                {#if team.channelId && team.roleId}
+                  <p>Channel ID: {team.channelId}</p>
+                  <p>Role ID: {team.roleId}</p>
+                {:else}
+                  <p>Discord not connected</p>
+                {/if}
+              </div>
+            </div>
 
-    <h2>Actions</h2>
-    <form>
-      <label for="newName">Change team name</label>
-      <input type="text" name="newName">
-      <input type="submit" value="Change">
-    </form>
-    {#if !team.isCommittee}
-    <button class="delete" on:click={handleTeamDelete}>{loadingTeamDelete ? "Loading..." : "Delete"}</button>
-    {/if}
-  {/if}
+            <div class="col-4">
+              <h2>Actions</h2>
+              <form class="mb-3">
+                <label for="newName" class="form-label">Change team name</label>
+                <input type="text" name="newName" class="form-control mb-3" />
+                <input
+                  type="submit"
+                  value="Change team name"
+                  class="btn btn-large btn-primary"
+                />
+              </form>
+              {#if !team.isCommittee}
+                <button class="btn btn-danger" on:click={handleTeamDelete}
+                  >{loadingTeamDelete ? "Loading..." : "Delete team"}</button
+                >
+              {/if}
+            </div>
+          {/if}
+        </div>
+      </GlassCard>
+    </div>
+  </div>
 </main>
 
 <style lang="scss">
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  
-  main {
-    padding: 2rem;
+  //   * {
+  //     margin: 0;
+  //     padding: 0;
+  //     box-sizing: border-box;
+  //   }
 
-    hr {
-      margin: 1rem;
-    }
+  //   main {
+  //     padding: 2rem;
 
-    .error {
-      color: red;
-    }
+  //     hr {
+  //       margin: 1rem;
+  //     }
 
-    .user, .data {
-      padding: 0.5rem;
-      margin: 0.5rem;
-      background-color: lightblue;
-      width: fit-content;
-    }
+  //     .error {
+  //       color: red;
+  //     }
 
-    form {
-      display: flex;
-      flex-direction: column;
-      width: fit-content;
-      gap: 5px;
-      margin: 0.5rem 0;
-    }
+  //     .user, .data {
+  //       padding: 0.5rem;
+  //       margin: 0.5rem;
+  //       background-color: lightblue;
+  //       width: fit-content;
+  //     }
 
-    .delete {
-      background-color: red;
-      color: white;
-      padding: 5px;
-      margin-top: 1rem;
-      font-size: 0.7rem;
-    }
-  }
+  //     form {
+  //       display: flex;
+  //       flex-direction: column;
+  //       width: fit-content;
+  //       gap: 5px;
+  //       margin: 0.5rem 0;
+  //     }
+
+  //     .delete {
+  //       background-color: red;
+  //       color: white;
+  //       padding: 5px;
+  //       margin-top: 1rem;
+  //       font-size: 0.7rem;
+  //     }
+  //   }
 </style>
